f32c6fd323ea2b8c6170377506f4f553eff5c8cc
coolme200 <2001-wms@163.com>
2019-03-21 16:25:19 +0800

    fix: enum item name default use

--- test/enums.test.js
+++ test/enums.test.js
@@ -62,6 +62,67 @@ describe('test/enums.test.js', function() {
     }]);
   });
 
+  it('new Enums() $name', function() {
+    var status = new Enums([{
+      $name: '$CLOSED',
+      name: 'CLOSED',
+      code: '1',
+      message: 'Shop close',
+    }, {
+      $name: '$OPENED',
+      name: 'OPENED',
+      code: '2',
+      message: 'Shop open',
+    }]);
+    assert(status);
+    console.log(status.CLOSED);
+    console.log(status.OPENED);
+    assert(Object.prototype.hasOwnProperty.call(status, '$CLOSED'));
+    assert(Object.prototype.hasOwnProperty.call(status, '$OPENED'));
+    assert.deepEqual('CLOSED', status.$CLOSED.name);
+    assert.deepEqual('OPENED', status.$OPENED.name);
+    assert(Object.prototype.hasOwnProperty.call(status.$CLOSED, 'code'));
+    assert(Object.prototype.hasOwnProperty.call(status.$CLOSED, 'ordinal'));
+    assert(Object.prototype.hasOwnProperty.call(status.getByCode('1'), 'name'));
+    assert(
+      Object.prototype.hasOwnProperty.call(status.getBy('name', 'CLOSED'), 'name')
+    );
+    assert(status.$CLOSED.eql(status.$CLOSED));
+    assert(!status.$CLOSED.eql(status.$OPENED));
+    assert(!status.$CLOSED.eql(null));
+    assert(!status.$CLOSED.eql({}));
+
+    var status2 = new Enums(status);
+    assert(Object.prototype.hasOwnProperty.call(status2, '$CLOSED'));
+
+    assert.deepEqual(status2.values(), [{
+      $name: '$CLOSED',
+      name: 'CLOSED',
+      code: '1',
+      message: 'Shop close',
+      ordinal: 0,
+    }, {
+      $name: '$OPENED',
+      name: 'OPENED',
+      code: '2',
+      message: 'Shop open',
+      ordinal: 1,
+    }]);
+
+    assert(status2.getBy('name', 'OPENING') === null);
+    assert.deepEqual(new Enums().values(), []);
+    assert.deepEqual(new Enums(['A', 'B', 'C']).values(), [{
+      name: 'A',
+      ordinal: 0,
+    }, {
+      name: 'B',
+      ordinal: 1,
+    }, {
+      name: 'C',
+      ordinal: 2,
+    }]);
+  });
+
   it('should new Enums twice', function() {
     var enums = [{
       name: 'CLOSED',
